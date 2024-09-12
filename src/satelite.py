8e13535c73ea0ab0f34a6b708b662330bacd8322
jacobo <jacobo.restrepoc@upb.edu.co>
2024-09-12 08:50:37 -0500

    se hace la primer version del codigo satelite, se deve coregir el error en el bucle for

--- src/satelite.py
+++ src/satelite.py
@@ -1,7 +1,17 @@
 
 def main():
-    #Tu código va aquí
-    pass # borra esta línea cuando con inicies tu código
+    altura_inicial=float(input("determine la altura inicial del satelite: "))
+    coeficiente_arrastre=float(input("cual es el cooeficiente de arastre con el que se movera la aeronave: "))
+    altura_minima=float(input("determine la altura minima de seguridad"))
+    altitud_orbita=altura_inicial
+    orbitas=0
+    for altura_minima in range (altitud_orbita): 
+        altitud_desendida=altura_inicial*coeficiente_arrastre
+        altitud_orbita=-altitud_desendida
+        coeficiente_arrastre=coeficiente_arrastre+1e-5
+        orbitas+=1
+        print(f"para la orbita {orbitas} se descendieron {altitud_desendida}km, su alt final es {altitud_orbita} y su nuevo coef de arraste es{coeficiente_arrastre}")
+    print ("simulacion finalizada")
 
 
 if __name__ == "__main__":
